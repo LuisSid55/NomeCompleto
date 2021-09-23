317068f244dd3c29967e3d59e8f8740e081b7caf
LuisSid55 <luiscitty4@gmail.com>
2021-09-23 02:35:46 -0300

    Adicionei as pergutas e respostas

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Nome;
+            String Sobre;
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write("Digite seu nome: ");
+            Console.ResetColor();
+            Nome = Console.ReadLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Digite seu sobrenome: ");
+            Console.ResetColor();
+            Sobre = Console.ReadLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write("Formato Padrão:   ");
+            Console.ResetColor();
+            Console.WriteLine($"{Nome} {Sobre}");
+            Sobre = Sobre.ToUpper();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Formato Catálogo: ");
+            Console.ResetColor();
+            Console.WriteLine($"{Sobre}, {Nome}");
+            Console.WriteLine();
+        }
+    }
+}
